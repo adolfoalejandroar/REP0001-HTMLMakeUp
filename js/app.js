47c3de3cad0baa0deefd2060b3f4400356131e58
Adolfo Alejandro Arenas Ramos <adolfoalejandroar@ufps.edu.co>
2025-03-11 23:56:42 -0500

    adding de review panel for all users, model only for now

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/**
+ * Esta función carga los trabajadores
+ */
 document.addEventListener('DOMContentLoaded', async () => {
 
     const studentsList = document.getElementById('studentsList');
@@ -15,8 +18,62 @@ document.addEventListener('DOMContentLoaded', async () => {
             clone.querySelector('.userName').textContent = student.name;
             clone.querySelector('.email').textContent = student.email;
             clone.querySelector('.avatar').src = student.photo;
-            clone.querySelector('.github').href = student.github_link;
             clone.querySelector('.code').textContent = student.code;
+
+            clone.querySelector('#github-btn').addEventListener('click', () => {
+                window.open(student.github_link, '_blank');
+            });
+
+            clone.querySelector('#edit-btn').addEventListener('click', () => {
+                const modalTemplate = document.getElementById('studentEditCard');
+                const modalClone = modalTemplate.content.cloneNode(true);
+                document.body.appendChild(modalClone);
+
+                const modal = document.querySelector('.edit-profile-panel');
+
+                modal.querySelector('#edit-name').value = student.name;
+                modal.querySelector('h2').textContent += ` ${student.code}`;
+                modal.querySelector('#edit-email').value = student.email;
+                modal.querySelector('#edit-github').value = student.github_link;
+                modal.querySelector('#edit-img').value = student.photo;
+                modal.querySelector('#edit-descript').value = student.description;
+
+                modal.querySelector('.close').addEventListener('click', () => {
+                    modal.style.display = 'none';
+                    document.body.removeChild(modal);
+                });
+
+                modal.querySelector('#edit').addEventListener('click', async (event) => {
+                    event.preventDefault();
+
+                    const updatedStudent = {
+                        name: modal.querySelector('#edit-name').value,
+                        email: modal.querySelector('#edit-email').value,
+                        github_link: modal.querySelector('#edit-github').value,
+                        photo: modal.querySelector('#edit-img').value,
+                        description: modal.querySelector('#edit-descript').value
+                    };
+
+                    try {
+                        await api.updateStudent(student.code, updatedStudent);
+                        alert('Student updated successfully!');
+                        modal.style.display = 'none';
+                        document.body.removeChild(modal);
+                        await renderStudents();
+                    } catch (error) {
+                        console.error('Error updating student:', error);
+                        alert('Failed to update student.');
+                    }
+                });
+            });
+
+            clone.querySelector('#info-btn').addEventListener('click', async (event) => {
+                console.log('info-btn');
+                document.body.classList.add('fade-out');
+                setTimeout(function () {
+                    window.location.href = './viewProf.html';
+                }, 1000);
+            });
 
             studentsList.appendChild(clone);
         });
@@ -27,6 +84,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 });
 
+
+
+/**
+ * Esta función crea un trabajador. Como no se ha cargado, funciona...
+ */
 document.getElementById('submit').addEventListener('click', async (event) => {
     event.preventDefault();
 
@@ -57,3 +119,36 @@ document.getElementById('submit').addEventListener('click', async (event) => {
         alert('Failed to create student.');
     }
 });
+
+
+/**
+ * ESTA FUNCIÓN NUNCA SE EJECUTA, SUPRIMIR, YA SE LLAMA EN EL DOMContentLoaded
+ */
+// document.getElementById('edit').addEventListener('click', async (event) => {
+//     event.preventDefault();
+
+//     const name = document.getElementById('edit-name').value;
+//     const code = document.getElementById('edit-code').value;
+//     const email = document.getElementById('edit-email').value;
+//     const github = document.getElementById('edit-github').value;
+//     const photo = document.getElementById('edit-photourl').value;
+//     const description = document.getElementById('edit-description').value;
+
+//     const student = {
+//         name,
+//         code,
+//         email,
+//         github_link: github,
+//         photo,
+//         description
+//     };
+
+//     try {
+//         await api.updateStudent(code, student);
+//         alert('Student updating successfully!');
+//         window.location.href = './index.html';
+//     } catch (error) {
+//         console.error('Error creating student:', error);
+//         alert('Failed to create student.');
+//     }
+// });
