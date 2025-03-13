aebb77ef1e5c3318723ddf407d5bbc1ee2799824
adolfoalejandroar <adolfoalejandroar@ufps.edu.co>
2025-03-13 01:26:20 -0500

    now the page can show the technologies by each student

--- js/view.js
+++ js/view.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const student = JSON.parse(localStorage.getItem('entityToView'));
     if (student) {
         document.getElementById('view-avatar').src = student.photo;
@@ -7,7 +7,35 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('view-code').textContent = `ID: ${student.code}`;
         document.getElementById('view-email').textContent = student.email;
         document.getElementById('view-description').textContent = student.description;
-    } else {
-        console.error('No student data found in localStorage');
     }
+
+    const techList = document.getElementById('tech-display');
+    const template = document.getElementById('tech-template');
+
+    async function renderTechs() {
+        techList.innerHTML = '';
+        const technologies = await api.getStudentTechnologies(student.code);
+        technologies.forEach((tech) => {
+            const clone = template.content.cloneNode(true);
+            clone.querySelector('.tech-name').textContent = tech.technology.name;
+            clone.querySelector('.tech-icon').src = tech.technology.image;
+            // clone.querySelector('.tech-image').src = tech.technology.image;
+            // clone.querySelector('.tech-image').alt = tech.technology.name;
+
+            /**
+             * JSON = tech {level, technology {name, image, code}
+             */
+
+            const stars = clone.querySelectorAll('.fa-star');
+            for (let i = 0; i < stars.length; i++) {
+                if (i < tech.level) {
+                    stars[i].classList.add('checked');
+                } else {
+                    stars[i].classList.remove('checked');
+                }
+            }
+            techList.appendChild(clone);
+        });
+    }
+    await renderTechs();
 });
